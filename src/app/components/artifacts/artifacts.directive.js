de3aa691fe9598d0f5b77c0d309148b69f72641b
Victor Gomez <vimagox@gmail.com>
2016-06-14 07:00:58 -0600

    Artifacts template updates

--- src/app/components/artifacts/artifacts.directive.js
+++ src/app/components/artifacts/artifacts.directive.js
@@ -17,10 +17,37 @@ export function ArtifactsDirective() {
 }
 
 class ArtifactsController {
-  constructor ($log, $state) {
+  constructor ($log, $state, $scope, $window) {
     'ngInject';
+
+    var colors = {
+      propuestas: '#2ea3ff',
+      quejas: '#ff642f',
+      foros: '#fcc000'
+    };
 
     this.$log = $log;
     this.$state = $state;
+    this.sublink = 1;
+    this.level = 'Nacional';
+
+    this.expanded = true;
+    this.xcolor = colors[this.title.toLowerCase()]
+
+    let w = angular.element($window);
+
+    $scope.getWindowDimensions = function () {
+      return { 'h': w.height(), 'w': w.width() };
+    };
+
+    $scope.$watch($scope.getWindowDimensions, newValue => {
+      this.windowHeight = newValue.h;
+      this.windowWidth = newValue.w;
+    }, true);
+
+    w.bind('resize', function () {
+      $scope.$apply();
+    });
+
   }
 }
